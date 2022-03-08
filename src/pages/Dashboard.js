7fd54d913e33569ce2b8708d2174a510eb46a319
ghighe <ghighe88@gmail.com>
2022-03-08 18:32:40 +0200

    start working dashboard

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,6 +1,62 @@
+import TinderCard from "react-tinder-card";
+import { useState } from "react";
 
 export const Dashboard = () => {
-    return (
-      <div>Dashboard page</div>
-    )
-}
+  const characters = [
+    {
+      name: "Richard Hendricks",
+      url: "https://i.imgur.com/Q9WPlWA.jpeg"
+    },
+    {
+      name: "Erlich Bachman",
+      url: "https://i.imgur.com/Q9WPlWA.jpeg"
+    },
+    {
+      name: "Monica Hall",
+      url: "https://i.imgur.com/Q9WPlWA.jpeg"
+    },
+    {
+      name: "Jared Dunn",
+      url: "https://i.imgur.com/Q9WPlWA.jpeg"
+    },
+    {
+      name: "Dinesh Chugtai",
+      url: "https://i.imgur.com/Q9WPlWA.jpeg"
+    }
+  ];
+  const [lastDirection, setLastDirection] = useState();
+
+  const swiped = (direction, nameToDelete) => {
+    console.log("removing: " + nameToDelete);
+    setLastDirection(direction);
+  };
+
+  const outOfFrame = (name) => {
+    console.log(name + " left the screen!");
+  };
+
+  return (
+    <div className="dashboard">
+      {/* <ChatContainer /> */}
+      <div className="swiper-container">
+        <div className="card-container">
+          {characters.map((character) => (
+            <TinderCard
+              className="swipe"
+              key={character.name}
+              onSwipe={(dir) => swiped(dir, character.name)}
+              onCardLeftScreen={() => outOfFrame(character.name)}
+            >
+              <div
+                style={{ backgroundImage: "url(" + character.url + ")" }}
+                className="card"
+              >
+                <h3>{character.name}</h3>
+              </div>
+            </TinderCard>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
